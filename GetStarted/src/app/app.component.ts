76d3f5561f24c0d12267916d2732671b893f61bd
Agnei SilvaAoioiuoGneiasdask <agnei.system@hotmail.com>
2018-09-22 22:36:19 -0300

    created a AppComponet manually

--- GetStarted/src/app/app.component.ts
+++ GetStarted/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component  } from '@angular/core'
+ 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector:'app-root ', 
+ template:'<div> \
+              <h1> Welcome to {{pageTitle}}!! </h1> \
+                   ....Started Files.... \
+            </div>'
 })
-export class AppComponent {
-  title = 'GetStarted';
+
+export class AppComponent{
+  
+  pageTitle: string = "Coorte management product";
+
 }
